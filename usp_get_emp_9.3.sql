8fcce06d24931b6fffcdf8e536d2310fa2f1a0ee
pulmuone <42885949+pulmuone@users.noreply.github.com>
2019-03-29 16:46:08 +0900

    Add files via upload

---
+++ usp_get_emp_9.3.sql
@@ -0,0 +1,25 @@
+﻿CREATE OR REPLACE FUNCTION public.usp_get_emp (
+  p_emp_nm varchar
+)
+RETURNS json AS
+$body$
+/*
+	ROW TO JSON
+*/
+
+    select
+    	json_agg(TO_JSON(tmp))
+    from
+    (
+        SELECT a.emp_id, a.emp_nm FROM public.employee a
+        WHERE a.emp_nm = CASE WHEN p_emp_nm  = '' THEN a.emp_nm ELSE p_emp_nm END
+    ) tmp;
+
+$body$
+LANGUAGE 'sql'
+VOLATILE
+CALLED ON NULL INPUT
+SECURITY INVOKER
+COST 100;
+
+
